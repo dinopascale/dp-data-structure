1357d09e686e101fe851459ffeca2d9072703bb3
Dino Pascale <dino.pascale@onedaygroup.it>
2020-10-03 19:12:04 +0200

    edit index.spec.ts for coverage 100%

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,5 +1,9 @@
-import {sum} from '../index';
+import {sum, multiply} from '../index';
 
 test('Sum 4 + 5 = 9', () => {
     expect(sum(4,5)).toBe(9)
+})
+
+test('Multiply 3 * 5 = 15', () => {
+    expect(multiply(3, 5)).toBe(15);
 })
